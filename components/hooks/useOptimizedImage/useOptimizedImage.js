76467c91c712488866ece0c289260dfe5fbb8c59
Carlos Martins <luacmartins@gmail.com>
2021-03-12 13:40:29 -0700

    fix: update image loader to reflect domain change

--- components/hooks/useOptimizedImage/useOptimizedImage.js
+++ components/hooks/useOptimizedImage/useOptimizedImage.js
@@ -1,7 +1,9 @@
 export default function useOptimizedImage(data) {
   const domain = process.env.NEXT_PUBLIC_DOMAIN
-  const srcRegex = /http:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)"/gm
-  const srcSetRegex = /https:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)\s(\d+)/gm
+  const srcRegex = /https?:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)"/gm
+  const srcRegex2 = /https?:\/\/server.ourglobaltrek.com\/wp-content\/uploads\/(.*?)"/gm
+  const srcSetRegex = /https?:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)\s(\d+)/gm
+  const srcSetRegex2 = /https?:\/\/server.ourglobaltrek.com\/wp-content\/uploads\/(.*?)\s(\d+)/gm
 
   let result
 
@@ -11,7 +13,17 @@ export default function useOptimizedImage(data) {
   )
 
   result = result.replace(
+    srcRegex2,
+    `https://cdn.statically.io/img/${domain}/f=auto,h=560/wp-content/uploads/$1"`
+  )
+
+  result = result.replace(
     srcSetRegex,
+    `https://cdn.statically.io/img/${domain}/f=auto,w=$1/wp-content/uploads/$1 $2`
+  )
+
+  result = result.replace(
+    srcSetRegex2,
     `https://cdn.statically.io/img/${domain}/f=auto,w=$1/wp-content/uploads/$1 $2`
   )
 

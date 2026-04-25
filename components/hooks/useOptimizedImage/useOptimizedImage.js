10ba8d98d9e4fc0c0ad34630b1a6301aee9814e0
Carlos Martins <cmartins@expensify.com>
2026-04-25 12:21:01 -0600

    rm server mentions

--- components/hooks/useOptimizedImage/useOptimizedImage.js
+++ components/hooks/useOptimizedImage/useOptimizedImage.js
@@ -1,9 +1,7 @@
 export default function useOptimizedImage(data) {
   const srcRegex = /https?:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)"/gm
-  const srcRegex2 = /https?:\/\/server.ourglobaltrek.com\/wp-content\/uploads\/(.*?)"/gm
   const srcRegex3 = /\/travel-blog\/wp-content\/uploads\/(.*?)"/gm
   const srcSetRegex = /https?:\/\/www.ourglobaltrek.com\/wp-content\/uploads\/(.*?)\s(\d+)/gm
-  const srcSetRegex2 = /https?:\/\/server.ourglobaltrek.com\/wp-content\/uploads\/(.*?)\s(\d+)/gm
   const srcSetRegex3 = /\/travel-blog\/wp-content\/uploads\/(.*?)\s(\d+)/gm
 
   let result
@@ -14,22 +12,12 @@ export default function useOptimizedImage(data) {
   )
 
   result = result.replace(
-    srcRegex2,
-    `/images/$1"`
-  )
-
-  result = result.replace(
     srcRegex3,
     `/images/$1"`
   )
 
   result = result.replace(
     srcSetRegex,
-    `/images/$1 $2`
-  )
-
-  result = result.replace(
-    srcSetRegex2,
     `/images/$1 $2`
   )
 
